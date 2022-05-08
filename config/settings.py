6420ecd982ba3382e2fa3394423bfbfd92524718
JagiJagi <jagijagiable@gmail.com>
2022-05-08 20:22:37 +0300

    Update Custom user

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-4-!7_)-6chr9+ha)u5m-_$89va16^33lb_qt#&rxb14g5(cb*#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -116,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -128,7 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -139,16 +134,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 
-
 SIMPLE_JWT = {
-   # 'AUTH_HEADER_TYPES': ('JWT',),
+    # 'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-#AUTH_USER_MODEL = 'profiles.UserNet'
+AUTH_USER_MODEL = 'profiles.UserNet'
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,15 +1,21 @@
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser
-#from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 
-#class UserNet(AbstractUser):
-class UserNet(AbstractBaseUser):
+class UserNet(AbstractUser):
     """Custom User Model"""
+    GENDER = (
+        ('male', 'male'),
+        ('female', 'female')
+    )
+
     middle_name = models.CharField(max_length=50)
-    first_login = models.DateTimeField(null=True)
+    first_login = models.DateTimeField(blank=True, null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
-
+    bio = models.TextField(blank=True, null=True)
+    github = models.CharField(max_length=500, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER, default='male')
